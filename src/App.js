0fb58d0e6a86465aaca347ab0b694d0a44c30dc8
{jainam} <{jainamgalar6s@gmail.com}>
2023-01-19 14:57:22 +0530

    Added fetch call to send the token to website

--- src/App.js
+++ src/App.js
@@ -12,11 +12,28 @@ function App() {
   //Generate Token
   const token = await getToken(messaging, { vapidkey: "BJ1vIvsIoz9XEqxMcVK8n1TWFf2vQJx4gsuqHQvNRI6SFGHqbcicUuL2eVi2RnPB5ZvX6s5PS9hS1kJGz8-u3JU" })
   console.log("Token Generated", token);
-  //send this token to database
+    var fcmToken = {
+      token: token
+    };
+    //send this token to database
+    fetch(window.origin.location+ "/saveToken", {
+    method: 'POST', 
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(fcmToken),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+    console.log('Success:', data);
+    })
+    .catch((error) => {
+    console.error('Error:', error);
+    }); 
   } else if (permission === 'denied') {
     alert("You have Disabled Notifications")
     }
-  }
+  };
 
   useEffect(() => {
     //req user for notification permission
